fc57106e8d612b01c4dc2b186291689084775ba6
Gor Davtyan <gor.davtyan@climatepartner.com>
2023-07-23 17:15:45 +0200

    feat: add routes logic

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { exampleRouter } from "@shared/server/api/routers/example";
 import { createTRPCRouter } from "@shared/server/api/trpc";
 import { topicRouter } from "@shared/server/api/routers/topics";
+import { noteRouter } from "./routers/note";
 
 /**
  * This is the primary router for your server.
@@ -10,8 +11,8 @@ import { topicRouter } from "@shared/server/api/routers/topics";
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
   topic: topicRouter,
+  note: noteRouter,
 });
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
- 
