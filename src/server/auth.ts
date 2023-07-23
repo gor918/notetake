cc8216c8fa9b15517be0f87161bc2ae38c176890
Gor Davtyan <gor.davtyan@climatepartner.com>
2023-07-23 16:05:04 +0200

    refactor: auth provider

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,7 +5,7 @@ import {
   type NextAuthOptions,
   type DefaultSession,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import GithubProvider from "next-auth/providers/github";
 import { env } from "@shared/env.mjs";
 import { prisma } from "@shared/server/db";
 
@@ -47,7 +47,7 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
+    GithubProvider({
       clientId: env.GITHUB_CLIENT_ID,
       clientSecret: env.GITHUB_CLIENT_SECRET,
     }),
